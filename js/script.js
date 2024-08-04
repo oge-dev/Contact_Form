cc50857d3256a6efe82d487146fc97a890e52c72
julietoge <julietogechi27@gmail.com>
2024-08-04 13:09:17 +0100

    update

--- js/script.js
+++ js/script.js
@@ -1,45 +1,52 @@
-const form = document.getElementById('form');
-const result = document.getElementById('result');
+document.addEventListener("DOMContentLoaded", function () {
+  const form = document.getElementById("form");
+  const result = document.getElementById("result");
 
-form.addEventListener('submit', function(e) {
+  form.addEventListener("submit", function (e) {
     e.preventDefault();
+
+    // Get the user's email input
+    const emailInput = document.getElementById("email").value;
+
+    // Set the `replyto` input field's value
+    document.getElementById("replyto").value = emailInput;
 
     const formData = new FormData(form);
     const object = Object.fromEntries(formData);
     const json = JSON.stringify(object);
 
-    result.innerHTML = "Please wait..."
+    result.innerHTML = "Please wait...";
 
-    fetch('https://api.web3forms.com/submit', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-                'Accept': 'application/json',
-            },
-            body: json
-        }).then(async (response) => {
-            let json = await response.json();
-            if (response.status == 200) {
-                result.innerHTML = json.message;
-                console.log(response);
-                setTimeout(() => {
-                    window.location.href = "https://web3forms.com/success"; // Custom success page URL
-                }, 2000);
-            } else {
-                console.log(response);
-                result.innerHTML = json.message;
-            }
-        })
-        .catch(error => {
-            console.log(error);
-            result.innerHTML = "Something went wrong!";
-        })
-        .then(
-            function() {
-            form.reset();
-            setTimeout(() => {
-                result.style.display = "none";
-            }, 3000);
+    fetch("https://api.web3forms.com/submit", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json",
+      },
+      body: json,
+    })
+      .then(async (response) => {
+        let json = await response.json();
+        if (response.status == 200) {
+          result.innerHTML = json.message;
+          console.log(response);
+          setTimeout(() => {
+            window.location.href = "https://web3forms.com/success"; // Custom success page URL
+          }, 2000);
+        } else {
+          console.log(response);
+          result.innerHTML = json.message;
         }
-    );
+      })
+      .catch((error) => {
+        console.log(error);
+        result.innerHTML = "Something went wrong!";
+      })
+      .then(function () {
+        form.reset();
+        setTimeout(() => {
+          result.style.display = "none";
+        }, 3000);
+      });
+  });
 });
